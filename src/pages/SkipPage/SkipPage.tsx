dcc026645942fb3b195537118d690a0d6688e57d
DarioKillenberger <s3787455@student.rmit.edu.au>
2023-10-14 22:09:02 +1100

    initial commit :>)

---
+++ src/pages/SkipPage/SkipPage.tsx
@@ -0,0 +1,27 @@
+import '../../css/SkipPage.css';
+import { useState } from 'react';
+
+function SkipPage() {
+    const [textVal, setText] = useState<String>();
+  
+    function clickHandle() {
+      setText("Would you really be the one to miss the show?");
+    }
+  
+  
+    return (
+      <>
+        <div className="inner-container">
+            <h1 id="nameText">Dario Killenberger</h1>
+            <h2 id="professionText">Software Engineering Student</h2>
+        </div>
+        <div className="topRightBox">
+          <button className="btn" onClick={clickHandle}>Skip all these gimmicks</button>
+        </div>
+        <h1>{textVal}</h1>
+      </>
+    );
+  }
+  
+  export default SkipPage;
+  
---
+++ src/pages/StartPage/StartPage.tsx
@@ -0,0 +1,27 @@
+import '../../css/StartPage.css';
+import { useState } from 'react';
+import SkipPage from '../SkipPage/SkipPage';
+
+function StartPage() {
+  const [textVal, setText] = useState<String>();
+
+  function clickHandle() {
+    setText("Would you really be the one to miss the show?");
+  }
+
+
+  return (
+    <>
+      <div className="inner-container">
+          <h1 id="nameText">Dario Killenberger</h1>
+          <h2 id="professionText">Software Engineering Student</h2>
+      </div>
+      <div className="topRightBox">
+        <button className="btn" onClick={clickHandle}>Skip all these gimmicks</button>
+      </div>
+      <SkipPage/>
+    </>
+  );
+}
+
+export default StartPage;
