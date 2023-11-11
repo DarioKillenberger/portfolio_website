bae7bd01eae4b15dedd7635818acce6472a1b594
DarioKillenberger <s3787455@student.rmit.edu.au>
2023-11-12 01:30:07 +1100

    worked on animations

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -54,10 +54,24 @@ function StartPage() {
           <div id="professionText">{parse(professionText)}</div>
       </div>
       <div className="topRightBox">
-        <button className="btn" onClick={changeText}>Skip all these gimmicks</button>
+        <div className='skipBtn'>
+          <svg xmlns="http://www.w3.org/2000/svg" id="uuid-ee995a3b-d96f-4d2f-bf17-25818921364f" data-name="Layer 1" viewBox="0 -10 80 70" onClick={() => navigate("/projects")}>
+            <path className="skipBtn2" d="m23.76,5.1c0-3.88,4.2-6.3,7.56-4.36l37.38,21.6c3.36,1.94,3.36,6.79,0,8.73l-37.4,21.57c-3.36,1.94-7.56-.49-7.56-4.37l.02-43.17Z"/>
+            <path className="skipBtn3" d="m.02,5.1C.02,1.22,4.23-1.2,7.58.74l37.38,21.6c3.36,1.94,3.36,6.79,0,8.73L7.56,52.64C4.2,54.58,0,52.15,0,48.27L.02,5.1Z"/>
+            <path className="skipBtn1" d="m23.76,5.04c0-3.88,4.2-6.3,7.56-4.36l37.38,21.6c3.36,1.94,3.36,6.79,0,8.73l-37.4,21.57c-3.36,1.94-7.56-.49-7.56-4.37l.02-43.17Z"/>7
+          </svg>
+          {/* <label htmlFor='skipBtn'>Skip all these gimmicks</label> */}
+        </div>
       </div>
-      <div className="bottomRightBox">
-         <button className="btn skipBtn" onClick={() => navigate("/wordGuess")}>START</button>
+      <div className="bottomMiddleBox">
+        <div className='playBtn'>
+          <svg  id="uuid-dd92ba08-e66e-41fe-b0aa-7068c11a02fc" data-name="Layer 1" viewBox="-25 -10 90 80" onClick={() => navigate("/wordGuess")}>
+            <path className="playBtn3" d="m.02,5.04C.02,1.17,4.23-1.26,7.58.68l37.38,21.6c3.36,1.94,3.36,6.79,0,8.73L7.56,52.58C4.2,54.52,0,52.1,0,48.22L.02,5.04Z"/>
+            <path className="playBtn2" d="m.02,5.04C.02,1.17,4.23-1.26,7.58.68l37.38,21.6c3.36,1.94,3.36,6.79,0,8.73L7.56,52.58C4.2,54.52,0,52.1,0,48.22L.02,5.04Z"/>
+            <path className="playBtn1" d="m.02,5.04C.02,1.17,4.23-1.26,7.58.68l37.38,21.6c3.36,1.94,3.36,6.79,0,8.73L7.56,52.58C4.2,54.52,0,52.1,0,48.22L.02,5.04Z"/>
+          </svg>
+          {/* <label htmlFor='playBtn'>Start</label> */}
+        </div>
       </div>
     </>
   );
